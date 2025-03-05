e941ebc25288a293549f473bb80b25649d2affba
Gilian <35449373+GilianJuice@users.noreply.github.com>
2025-03-05 09:18:11 +0100

    Moved all Evo PDF specific code to a separate class library (#1)
    * New package for Evo PDF code, so that the GCL is no longer directly dependent on it.
    
    * Added documentation.
    
    * Apperantly version 11.4.2 of EvoPdf only exists for their Windows variant, not Linux.

---
+++ GeeksCoreLibrary.Modules.GclConverters.EvoPdf/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using GeeksCoreLibrary.Modules.GclConverters.EvoPdf.Services;
+using GeeksCoreLibrary.Modules.GclConverters.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GeeksCoreLibrary.Modules.GclConverters.EvoPdf.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Extension method to add our <see cref="EvoPdfHtmlToPdfConverterService"/> implementation of <see cref="IHtmlToPdfConverterService"/> to the service collection.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <returns>The same service collection.</returns>
+    // ReSharper disable once UnusedMethodReturnValue.Global
+    public static IServiceCollection AddEvoPdfHtmlToPdfConverterService(this IServiceCollection services)
+    {
+        return services.AddTransient<IHtmlToPdfConverterService, EvoPdfHtmlToPdfConverterService>();
+    }
+}
